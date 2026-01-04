cf0fe56fd4422e7163c92312c28948620a4a3dcb
deannos <amjha21122002@gmail.com>
2026-01-05 00:06:29 +0530

    feat: wire POST /employees endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/deannos/incubyte-sm-kata-deannos/internal/db"
+	"github.com/deannos/incubyte-sm-kata-deannos/internal/employee"
 )
 
 func main() {
-	log.Println("starting server on :8080")
+	database, err := db.NewInMemoryDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = database.Exec(`
+		CREATE TABLE IF NOT EXISTS employees (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			full_name TEXT,
+			job_title TEXT,
+			country TEXT,
+			salary REAL
+		)
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repo := employee.NewRepository(database)
+	service := employee.NewService(repo)
+	handler := employee.NewHandler(service)
+
+	http.HandleFunc("/employees", handler.CreateEmployee)
+
+	log.Println("server running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
