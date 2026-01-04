620682562203194aa0f6e2e88dd58962580cc2d3
deannos <amjha21122002@gmail.com>
2026-01-04 18:19:37 +0530

    feat: implement find employee by id

--- internal/employee/repository.go
+++ internal/employee/repository.go
@@ -27,5 +27,16 @@ func (r *Repository) Save(e Employee) (int64, error) {
 }
 
 func (r *Repository) FindByID(id int64) (Employee, error) {
-	return Employee{}, nil
+	row := r.db.QueryRow(
+		`SELECT id, full_name, job_title, country, salary
+		 FROM employees WHERE id = ?`, id,
+	)
+
+	var e Employee
+	err := row.Scan(&e.ID, &e.FullName, &e.JobTitle, &e.Country, &e.Salary)
+	if err != nil {
+		return Employee{}, err
+	}
+
+	return e, nil
 }
